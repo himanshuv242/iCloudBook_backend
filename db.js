764cedb29bfa1bb15bae5c76d1883f05d54ff652
Himanshu Verma <himanshuv242cp@gmail.com>
2023-01-23 02:52:48 +0530

    Validating user request and showing relevant msg

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://localhost:27017/"
+const mongoURI = "mongodb://localhost:27017/cloud_notebook"
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI, ()=>{
