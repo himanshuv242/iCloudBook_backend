0b12d89c33102d57aa6cb022d3f38087a220929c
Himanshu Verma <himanshuv242cp@gmail.com>
2023-01-23 01:46:23 +0530

    Refactored the folder structure and made routes for end point hitting

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const mongoURI = "mongodb://localhost:27017/"
 
-const connectToMongo = async() =>{
+const connectToMongo = () =>{
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected to mongo successfully");
     })
