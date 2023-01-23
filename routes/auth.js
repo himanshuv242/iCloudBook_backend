a4dd8991bdf362d619d5f2e8507d2cd4aefc9459
Himanshu Verma <himanshuv242cp@gmail.com>
2023-01-24 03:01:00 +0530

    Added JWT authentication to generate token

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');//signature
+
+
+const JWT_SECRET = 'Harryisagoodb$oy'
 
 //Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser',[
@@ -23,13 +28,25 @@ router.post('/createuser',[
     if(user){
       return res.status(400).json({error:"Sorry a user with this mail already exists"})
     }
+    const salt = await bcrypt.genSalt(10);//to generate salt
+    const secPass = await bcrypt.hash(req.body.password,salt);//mark await cause it return promiss
+
+    //Saving data to database and creating a new user
     user = await User.create({
         name: req.body.name,
+        password: secPass,
         email: req.body.email,
-        password: req.body.password
-       })
+       });
+       const data = {
+        user:{
+          id:user.id
+        }
+       }
+       const authtoken = jwt.sign(data, JWT_SECRET);
 
-    res.json(user)
+    // res.json(user)
+    res.json({authtoken});
+
       } catch (error) {
         console.log(error.message);
         res.status(500).send("Some error occured")
