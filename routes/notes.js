1d8e5e6145550c24fd94c5ced8b53e85984566c4
Himanshu Verma <himanshuv242cp@gmail.com>
2023-01-30 01:11:27 +0530

    Added updating end point

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,7 @@ var fetchuser = require("../middleware/fetchuser");
 const Note = require("../models/Note");
 const { body, validationResult } = require("express-validator"); //to validate note if its not blank and we are still saving it to our data base.
 
-//ROUTE:1 Get All the Notes using: GET "/api/auth/fetchallnotes".Login Required
+//ROUTE:1 Get All the Notes using: GET "/api/notes/fetchallnotes".Login Required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   try {
     const notes = await Note.find({ user: req.user.id });
@@ -15,7 +15,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   }
 });
 
-//ROUTE:2 Add the Notes using: POST "/api/auth/addnote".Login Required
+//ROUTE:2 Add the Notes using: POST "/api/notes/addnote".Login Required
 router.post(
   "/addnote",
   fetchuser,
@@ -49,6 +49,32 @@ router.post(
       console.log(error.message);
       res.status(500).send("Internal server error");
     }
+  }
+);
+
+//ROUTE:3 UPDATE an existing note using: POST "/api/notes/updatenote". Login Required
+router.put(
+  "/updatenote/:id",//so that user update only there note not other's.
+  fetchuser,
+  async (req, res) => {
+    const {title, description, tag} = req.body;
+    //Create a newNote object
+    const newNote = {};
+    if(title){newNote.title=title};
+    if(description){newNote.description=description};
+    if(tag){newNote.tag=tag};
+
+    //Find the note to be update and update it.
+    let note = await Note.findById(req.params.id);
+    if(!note) {return res.status(404).send("Not Found")}//check if note exist
+
+    if(note.user.toString() !== req.user.id){  // to check if someone is not trying to access other user note.
+      return res. status(401).send("Not Allowed");
+    } 
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
+
   }
 );
 
